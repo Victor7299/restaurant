7cbef11fe89e021fbeb184a6ff5dfbd88f293433
Victor <vcitorramosfernando7299@gmail.com>
2022-03-11 01:06:21 -0500

    data from mockaroo

---
+++ dump_files/orders.sql
@@ -0,0 +1,15 @@
+insert into orders (id, table_id) values (1, 15);
+insert into orders (id, table_id) values (2, 8);
+insert into orders (id, table_id) values (3, 13);
+insert into orders (id, table_id) values (4, 15);
+insert into orders (id, table_id) values (5, 6);
+insert into orders (id, table_id) values (6, 14);
+insert into orders (id, table_id) values (7, 1);
+insert into orders (id, table_id) values (8, 13);
+insert into orders (id, table_id) values (9, 11);
+insert into orders (id, table_id) values (10, 2);
+insert into orders (id, table_id) values (11, 8);
+insert into orders (id, table_id) values (12, 10);
+insert into orders (id, table_id) values (13, 8);
+insert into orders (id, table_id) values (14, 5);
+insert into orders (id, table_id) values (15, 15);
