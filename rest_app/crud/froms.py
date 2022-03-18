73a29c64e6a4791b1156fed11d7d2db5f3b154ed
Victor <victorramosfernando@gmail.com>
2022-03-17 21:47:41 -0500

    crud templates

---
+++ rest_app/crud/froms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DecimalField
+from wtforms.validators import InputRequired
+
+class ProductForm(FlaskForm):
+    name = StringField(
+        label='Product Name',
+        validators=[
+            InputRequired(),
+        ],
+    )
+    price = DecimalField(
+        label='price',
+        validators=[
+            InputRequired()
+        ],
+        places=2,
+    )
+
+
+class TableForm(FlaskForm):
+    table_name = StringField(
+        label='Table Name',
+        validators=[
+            InputRequired(),
+        ],
+    )
